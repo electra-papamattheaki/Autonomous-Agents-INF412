39f86eefa86202038cd3889b7b409166b214df77
Electra <62039899+electra-papamattheaki@users.noreply.github.com>
2023-11-30 11:15:54 +0200

    Made the firsrt changes to Rat1.

--- Lab 2/ratslife/controllers/Rat1/Rat1.java
+++ Lab 2/ratslife/controllers/Rat1/Rat1.java
@@ -13,9 +13,7 @@
 // limitations under the License.
 
 import com.cyberbotics.webots.controller.Accelerometer;
-import com.cyberbotics.webots.controller.Camera;
 import com.cyberbotics.webots.controller.DistanceSensor;
-import com.cyberbotics.webots.controller.LED;
 import com.cyberbotics.webots.controller.LightSensor;
 import com.cyberbotics.webots.controller.Motor;
 import com.cyberbotics.webots.controller.Robot;
@@ -30,28 +28,19 @@ public class Rat1 extends Robot {
   protected final double[] slowMotionWeights = {0.0125,0.00625,0.0,0.0,0.0,0.0,0.00625,0.0125};
 
   protected Accelerometer accelerometer;
-  protected Camera camera;
-  protected int cameraWidth, cameraHeight;
   protected Motor leftMotor, rightMotor;
   protected DistanceSensor[] distanceSensors = new DistanceSensor[8];
   protected LightSensor[] lightSensors = new LightSensor[8];
-  protected LED[] leds = new LED[10];
 
   public Rat1() {
     accelerometer = getAccelerometer("accelerometer");
-    camera = getCamera("camera");
-    camera.enable(8*timeStep);
-    cameraWidth=camera.getWidth();
-    cameraHeight=camera.getHeight();
     leftMotor = getMotor("left wheel motor");
     rightMotor = getMotor("right wheel motor");
     leftMotor.setPosition(Double.POSITIVE_INFINITY);
     rightMotor.setPosition(Double.POSITIVE_INFINITY);
     leftMotor.setVelocity(0.0);
     rightMotor.setVelocity(0.0);
-    for (int i=0;i<10;i++) {
-      leds[i]=getLED("led"+i);
-    };
+    
     for (int i=0;i<8;i++) {
       distanceSensors[i] = getDistanceSensor("ps"+i);
       distanceSensors[i].enable(timeStep);
@@ -63,8 +52,6 @@ public class Rat1 extends Robot {
 
   public void run() {
 
-    int blink = 0;
-    int oldDx = 0;
     Random r = new Random();
     boolean turn = false;
     boolean right = false;
@@ -73,7 +60,6 @@ public class Rat1 extends Robot {
     double oldBattery = -1.0;
     int image[];
     double distance[] = new double[8];
-    int ledValue[] = new int[10];
     double leftSpeed, rightSpeed;
 
     while (step(timeStep) != -1) {
@@ -82,79 +68,50 @@ public class Rat1 extends Robot {
       image = camera.getImage();
       for(int i=0;i<8;i++) distance[i] = distanceSensors[i].getValue();
       battery = batterySensorGetValue();
-      for(int i=0;i<10;i++) ledValue[i] = 0;
 
       // obstacle avoidance behavior
       leftSpeed  = maxSpeed;
       rightSpeed = maxSpeed;
-      for (int i=0;i<8;i++) {
-        leftSpeed  -= (slowMotionWeights[i]+collisionAvoidanceWeights[i])*distance[i];
-        rightSpeed -= (slowMotionWeights[i]-collisionAvoidanceWeights[i])*distance[i];
+      //for (int i=0;i<8;i++) {
+        //leftSpeed  -= (slowMotionWeights[i]+collisionAvoidanceWeights[i])*distance[i];
+        //rightSpeed -= (slowMotionWeights[i]-collisionAvoidanceWeights[i])*distance[i];
+      //}
+      /// To detect an obstacle use sensors 3,4
+      if(distance[3] > 500 || distance[4] > 500) {
+        leftSpeed = maxSpeed;  
+        rightSpeed = -maxSpeed; // Turn Right.
       }
-      // return either to left or to right when there is an obstacle
-      if (distance[6]+distance[7] > 1800 || distance[0]+distance[1] > 1800) {
-        if (!turn) {
-          turn = true;
-          right = r.nextBoolean();
+      else { 
+        //The distance from the left hand side wall is okay, i obey to the LWF rule continue straight
+        if(distance[5] > 300) {
+          leftSpeed = maxSpeed;
+          rightSpeed = maxSpeed;
         }
-        if (right) {
-          ledValue[2] = 1;
-          leftSpeed  =  maxSpeed;
-          rightSpeed = -maxSpeed;
-        } else {
-          ledValue[6] = 1;
-          leftSpeed  = -maxSpeed;
-          rightSpeed =  maxSpeed;
+        else {
+          leftSpeed = maxSpeed/8;
+          rightSpeed = maxSpeed;
         }
-      } else {
-        turn=false;
-      }
-      // vision
-      int blobX=0,blobY=0,blobCounter=0;
-      // looking for an alight feeder
-      for(int x=0; x<cameraWidth; x++) for(int y=cameraWidth/3; y<2*cameraWidth/3; y++) {
-        int pixel = image[y * cameraWidth + x];
-        if (Camera.pixelGetGreen(pixel) >= 248 &&
-            Camera.pixelGetBlue(pixel) >= 248) {
-          blobX += x;
-          blobY += y;
-          blobCounter++;
+        
+        //I had an 180 left turn, let me fix my position parallel to the wall to avoid crashing to it
+        if(distance[5] > 300){
+          //System.out.println("Oh I came too close");
+          leftSpeed = maxSpeed/2.5;
+          rightSpeed = maxSpeed;
         }
       }
-      if (blobCounter > 2) { // significant enough
-        seeFeeder = true;
-        blobX /= blobCounter;
-        blobY /= blobCounter;
-        int dx = (blobX-cameraWidth/2)*10;
-        if (dx > 0) ledValue[1] = 1;
-        else ledValue[7] = 1;
-        if (oldDx != dx){
-          leftSpeed  =  2*dx;
-          rightSpeed = -2*dx;
-          oldDx = dx;
-        } else {
-          leftSpeed  = maxSpeed/3;
-          rightSpeed = maxSpeed/3;
-        }
-      }
+      
+      
+     
       //recharging behavior
       if (battery > oldBattery) {
         leftSpeed  = 0.0;
         rightSpeed = 0.0;
-        ledValue[8] = 1; // turn on the body led
       }
       oldBattery = battery;
-      if (blink++ >= 20) { // blink the back LEDs
-        ledValue[4] = 1;
-        if (blink == 40) blink = 0;
-      }
-
-      //set actuators
-      for(int i=0; i<10; i++) {
-        leds[i].set(ledValue[i]);
-      }
-      leftMotor.setVelocity(0.00628 * leftSpeed);
-      rightMotor.setVelocity(0.00628 * rightSpeed);
+      
+      // Added negative(-) to make the robot work backwards.
+      leftMotor.setVelocity(-0.00628 * leftSpeed);
+      rightMotor.setVelocity(-0.00628 * rightSpeed);
     }
     // Enter here exit cleanup code
   }
